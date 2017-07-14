6e81d76f2a0836c4feeb7134ffaeeb6e1617541a
Alex Crichton <alex@alexcrichton.com>
2017-07-14 06:09:31 -0700

    Use raw `isize` and `usize` constructors
    These have been added upstream!

--- src/unstable.rs
+++ src/unstable.rs
@@ -282,22 +282,8 @@ macro_rules! ints {
 }
 
 ints! {
-    u8, u16, u32, u64, /*usize,*/
-    i8, i16, i32, i64, /*isize,*/
-}
-
-macro_rules! ints_stringified {
-    ($($t:ident,)*) => {$(
-        impl From<$t> for Literal {
-            fn from(t: $t) -> Literal {
-                Literal(to_literal(&format!(concat!("{}", stringify!($t)), t)))
-            }
-        }
-    )*}
-}
-
-ints_stringified! {
-    usize, isize,
+    u8, u16, u32, u64, usize,
+    i8, i16, i32, i64, isize,
 }
 
 macro_rules! floats {
