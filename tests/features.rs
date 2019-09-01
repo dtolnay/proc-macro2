22b7710d5924e28892cf96777cf5462a7d5a81d7
David Tolnay <dtolnay@gmail.com>
2019-08-31 18:52:27 -0700

    Format with rustfmt 2019-08-19

--- tests/features.rs
+++ tests/features.rs
@@ -1,4 +1,8 @@
-#[test] #[ignore]
+#[test]
+#[ignore]
 fn make_sure_no_proc_macro() {
-    assert!(!cfg!(feature = "proc-macro"), "still compiled with proc_macro?");
+    assert!(
+        !cfg!(feature = "proc-macro"),
+        "still compiled with proc_macro?"
+    );
 }
