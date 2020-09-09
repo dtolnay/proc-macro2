cd8742356eb03fc35df3129acaa33db4a6ad6114
David Tolnay <dtolnay@gmail.com>
2020-09-09 10:50:54 -0700

    Consolidate marker trait handling into Marker type

---
+++ src/marker.rs
@@ -0,0 +1,14 @@
+use std::marker::PhantomData;
+use std::rc::Rc;
+
+// Zero sized marker with the correct set of autotrait impls we want all proc
+// macro types to have.
+pub(crate) type Marker = PhantomData<ProcMacroAutoTraits>;
+
+pub(crate) use self::value::*;
+
+mod value {
+    pub(crate) use std::marker::PhantomData as Marker;
+}
+
+pub(crate) struct ProcMacroAutoTraits(Rc<()>);
