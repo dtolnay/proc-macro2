74443a67958a91dd4331a790756fe228fe6011af
David Tolnay <dtolnay@gmail.com>
2023-03-11 14:29:39 -0800

    Add sizes of not(wrap_proc_macro) spans

--- tests/test_size.rs
+++ tests/test_size.rs
@@ -8,16 +8,30 @@ fn test_proc_macro_span_size() {
     assert_eq!(mem::size_of::<Option<proc_macro::Span>>(), 4);
 }
 
-#[cfg_attr(span_locations, ignore)]
+#[cfg_attr(not(all(not(wrap_proc_macro), not(span_locations))), ignore)]
 #[test]
-fn test_proc_macro2_span_size_without_locations() {
+fn test_proc_macro2_fallback_span_size_without_locations() {
+    assert_eq!(mem::size_of::<proc_macro2::Span>(), 0);
+    assert_eq!(mem::size_of::<Option<proc_macro2::Span>>(), 1);
+}
+
+#[cfg_attr(not(all(not(wrap_proc_macro), span_locations)), ignore)]
+#[test]
+fn test_proc_macro2_fallback_span_size_with_locations() {
+    assert_eq!(mem::size_of::<proc_macro2::Span>(), 8);
+    assert_eq!(mem::size_of::<Option<proc_macro2::Span>>(), 12);
+}
+
+#[cfg_attr(not(all(wrap_proc_macro, not(span_locations))), ignore)]
+#[test]
+fn test_proc_macro2_wrapper_span_size_without_locations() {
     assert_eq!(mem::size_of::<proc_macro2::Span>(), 4);
     assert_eq!(mem::size_of::<Option<proc_macro2::Span>>(), 8);
 }
 
-#[cfg_attr(not(span_locations), ignore)]
+#[cfg_attr(not(all(wrap_proc_macro, span_locations)), ignore)]
 #[test]
-fn test_proc_macro2_span_size_with_locations() {
+fn test_proc_macro2_wrapper_span_size_with_locations() {
     assert_eq!(mem::size_of::<proc_macro2::Span>(), 12);
     assert_eq!(mem::size_of::<Option<proc_macro2::Span>>(), 12);
 }
