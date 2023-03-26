f7c0435ce9d31d7e45b5a7ea66cd213db66e7a36
David Tolnay <dtolnay@gmail.com>
2023-03-26 10:13:41 -0700

    Add AFL++ support to fuzz target

--- fuzz/fuzz_targets/parse_token_stream.rs
+++ fuzz/fuzz_targets/parse_token_stream.rs
@@ -1,12 +1,30 @@
-#![no_main]
+#![cfg_attr(feature = "libfuzzer", no_main)]
 
-use libfuzzer_sys::fuzz_target;
 use std::str;
 
-fuzz_target!(|bytes: &[u8]| {
+#[cfg(not(any(
+    all(feature = "libfuzzer", not(feature = "afl")),
+    all(not(feature = "libfuzzer"), feature = "afl"),
+)))]
+fn main() {
+    compile_error! {
+        r#"exactly one of feature="libfuzzer" and feature="afl" must be enabled"#
+    }
+}
+
+#[cfg(feature = "libfuzzer")]
+libfuzzer_sys::fuzz_target!(|bytes: &[u8]| { do_fuzz(bytes) });
+
+#[cfg(feature = "afl")]
+fn main() {
+    let hook = true; // turn panic into crashes
+    afl::fuzz(hook, do_fuzz);
+}
+
+fn do_fuzz(bytes: &[u8]) {
     if bytes.len() < 200 {
         if let Ok(string) = str::from_utf8(bytes) {
             _ = string.parse::<proc_macro2::TokenStream>();
         }
     }
-});
+}
