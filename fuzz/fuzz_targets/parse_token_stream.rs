ba7ad432e58f4e77312619f2633de1713e23d4e5
David Tolnay <dtolnay@gmail.com>
2023-03-26 17:56:20 -0700

    Add honggfuzz support to fuzz target

--- fuzz/fuzz_targets/parse_token_stream.rs
+++ fuzz/fuzz_targets/parse_token_stream.rs
@@ -8,18 +8,37 @@
 //     cargo install afl
 //     cargo afl build --no-default-features --features afl --release
 //     cargo afl fuzz -i in -o out target/release/parse_token_stream
+//
+// honggfuzz:
+//
+//     cargo install honggfuzz
+//     cargo hfuzz build --no-default-features --features honggfuzz
+//     HFUZZ_RUN_ARGS="--threads $(nproc) --max_file_size 200 --timeout 1" cargo hfuzz run parse_token_stream
 
 #![cfg_attr(feature = "libfuzzer", no_main)]
 
 use std::str;
 
 #[cfg(not(any(
-    all(feature = "libfuzzer", not(feature = "afl")),
-    all(not(feature = "libfuzzer"), feature = "afl"),
+    all(
+        feature = "libfuzzer",
+        not(feature = "afl"),
+        not(feature = "honggfuzz")
+    ),
+    all(
+        not(feature = "libfuzzer"),
+        feature = "afl",
+        not(feature = "honggfuzz")
+    ),
+    all(
+        not(feature = "libfuzzer"),
+        not(feature = "afl"),
+        feature = "honggfuzz"
+    ),
 )))]
 fn main() {
     compile_error! {
-        r#"exactly one of feature="libfuzzer" and feature="afl" must be enabled"#
+        r#"exactly one of feature="libfuzzer" or feature="afl" or feature="honggfuzz" must be enabled"#
     }
 }
 
@@ -30,6 +49,13 @@ libfuzzer_sys::fuzz_target!(|bytes: &[u8]| { do_fuzz(bytes) });
 fn main() {
     let hook = true; // turn panic into crashes
     afl::fuzz(hook, do_fuzz);
+}
+
+#[cfg(feature = "honggfuzz")]
+fn main() {
+    loop {
+        honggfuzz::fuzz(do_fuzz);
+    }
 }
 
 fn do_fuzz(bytes: &[u8]) {
